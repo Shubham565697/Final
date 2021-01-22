0c1d6cd08b335b267175d0475d3b2ae201ad5141
Shubham565697 <razzshubham97@gmail.com>
2021-01-22 22:41:51 +0545

    fifth commit

--- app/src/main/java/com/example/medivex/sign_up.kt
+++ app/src/main/java/com/example/medivex/sign_up.kt
@@ -5,17 +5,5 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class sign_up : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-        var btnSign: Button = findViewById(R.id.btnSignUp)
-        var btnLogin: Button = findViewById(R.id.btnLogin)
-        btnSign.setOnClickListener() {
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
-        }
 
-
-    }
 }
