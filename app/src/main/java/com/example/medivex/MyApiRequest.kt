979a3b789025a4550fa5d56c46050e72a53472a1
Shubham565697 <razzshubham97@gmail.com>
2021-02-19 13:46:41 +0545

    Sixth commit

--- app/src/main/java/com/example/medivex/MyApiRequest.kt
+++ app/src/main/java/com/example/medivex/MyApiRequest.kt
@@ -19,12 +19,7 @@ abstract class MyApiRequest{
             val error = response.errorBody()?.string()
             val message = StringBuilder()
             error?.let {
-                try {
-                    message.append(JSONObject(it).getString("success"))
-                } catch (e: JSONException) {
-                }
-                message.append("\n")
-            }
+
             message.append("Error code : ${response.code()}")
             throw IOException(message.toString())
         }
