242a5f7b7938787fb4d6cf60b149b2ab3013746f
Shubham565697 <razzshubham97@gmail.com>
2021-02-19 14:15:39 +0545

    Second commit

--- app/src/main/java/com/example/medivex/Splash.kt
+++ app/src/main/java/com/example/medivex/Splash.kt
@@ -8,11 +8,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
 
-        CoroutineScope(Dispatchers.IO).launch {
-            delay(2000)
-            startActivity(Intent(this@Splash,login::class.java))
-        }
     }
 }
